b7dde5bdc0cd48b6a48e4beeab90b6d96bb3ac34
ElayGelbart <elaygelbart@gmail.com>
2022-01-09 17:08:31 +0200

    finish mission

--- ofer/index.js
+++ ofer/index.js
@@ -24,21 +24,6 @@ app.get("/", function (req, res) {
   res.send("Testing my server");
 });
 
-//  Creating first table "numbers"
-app.get("/create-table", function (req, res) {
-  const sql = `
-    CREATE TABLE IF NOT EXISTS numbers (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      number INT NOT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    ) ENGINE=INNODB;
-  `;
-  con.query(sql, function (err, result) {
-    if (err) throw err;
-    res.send("numbers table created");
-  });
-});
-
 app.get("/getTeacher/:teacherID", (req, res) => {
   const teacherID = Number(req.params.teacherID)
   const getTeacherQuery = `SELECT * FROM Teachers WHERE teacher_id=${teacherID}`
@@ -59,7 +44,7 @@ app.get("/getStudent/:studentID", (req, res) => {
 
 app.get("/getSubject/:subjectsName", (req, res) => {
   const subjectStringName = req.params.subjectsName
-  const getSubjectQuery = `SELECT * FROM Subjects WHERE subject_name=${subjectStringName}`
+  const getSubjectQuery = `SELECT * FROM Subjects WHERE subject_name='${subjectStringName}'`
   con.query(getSubjectQuery, (err, data) => {
     if (err) res.send('error happened')
     res.send(data)
@@ -68,8 +53,62 @@ app.get("/getSubject/:subjectsName", (req, res) => {
 
 app.get("/getClass/:className", (req, res) => {
   const className = req.params.className
-  const getClassQuery = `SELECT * FROM Class WHERE class_name=${className}`
+  const getClassQuery = `SELECT * FROM Classes WHERE class_name='${className}'`
   con.query(getClassQuery, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.post("/new/teacher", (req, res) => {
+  const { subject_name, class_name, teacher_age, teacher_fullname } = req.body
+  const postTeacherQuery = `INSERT INTO Teachers(subject_name,class_name,teacher_age,teacher_fullname) VALUES(${subject_name},${class_name},${teacher_age},${teacher_fullname})`
+  con.query(postTeacherQuery, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.post("/new/pupil", (req, res) => {
+  const { class_Name, subjects, student_name, student_age } = req.body
+  const postStudentQuery = `INSERT INTO Students(class_Name, subjects, student_name, student_age) VALUES(${class_Name},${subjects},${student_name},${student_age})`
+  con.query(postStudentQuery, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.put("/update/pupil/:pupilID", (req, res) => {
+  const { class_Name, subjects, student_name, student_age } = req.body
+  const updateStudentQuery = `UPDATE Students set class_Name='${class_Name}',subjects='${subjects}', student_name='${student_name}', student_age = '${student_age}'
+   WHERE student_id=${req.params.pupilID}`
+  con.query(updateStudentQuery, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.put("/update/class/:classID", (req, res) => {
+  const { class_name } = req.body
+  const updateClassQuery = `UPDATE Classes set class_name='${class_name}'
+   WHERE l=${req.params.classID}`
+  con.query(updateClassQuery, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.delete('/remove/pupil/:pupilID', (req, res) => {
+  const deleteStudentQ = `DELETE FROM Students Where student_id=${req.params.pupilID}`
+  con.query(deleteStudentQ, (err, data) => {
+    if (err) res.send('error happened')
+    res.send(data)
+  })
+})
+
+app.delete('/remove/teacher/:teacher', (req, res) => {
+  const deleteTeacherQ = `DELETE FROM Teachers Where teacher_id=${req.params.teacher}`
+  con.query(deleteTeacherQ, (err, data) => {
     if (err) res.send('error happened')
     res.send(data)
   })
